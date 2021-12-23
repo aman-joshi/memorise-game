23db9cce4f1f743833fd3dcb69f3631f31ac115a
Aman Joshi <aman.joshi@crownstack.com>
2021-12-23 08:21:48 +0530

    Initial Commit

---
+++ Memorise/MemoriseApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoriseApp.swift
+//  Memorise
+//
+//  Created by Aman Joshi on 23/12/21.
+//
+
+import SwiftUI
+
+@main
+struct MemoriseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
